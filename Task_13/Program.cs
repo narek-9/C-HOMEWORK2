63c971cb6ac862f13c90fa6a938d6d39fedf584e
Narek <narekazibekyan5@gmail.com>
2022-05-19 20:08:15 +0400

    third Digit

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(number);
+
+if(Number.Length < 3) {
+    Console.WriteLine("Третьей цифры нет.");
+}
+else {
+    Console.WriteLine("Третья цифра = " + Number[2]);
+}
+
